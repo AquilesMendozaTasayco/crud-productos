35043a3c0884be11ebafb9d96e9ee4892a0c983e
AquilesMendozaTasayco <aquilesfabianmendozatasayco@gmail.com>
2025-11-20 00:20:14 -0500

    Fix Dockerfile: add Node 20, remove sqlite issues, correct apache

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Gestión de Productos</title>
-<script src="http://cdn.tailwindcss.com"></script>
+<script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 <body class="bg-gradient-to-br from-gray-50 to-gray-100 min-h-screen py-10">
